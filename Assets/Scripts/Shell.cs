08d5be7f07c6a8291147acbc8dda920e6ea4ee64
Vitor-Borges <62852982+Vitor-Borges@users.noreply.github.com>
2022-12-23 01:36:09 +0000

    Aceleração
    Aceleração parte 2 completa;
    Alterei valores nas variaveis "massa" e "forca" pois caso contrário a bala não se moveria corretamente;
    alterei tambem valores no scrip que define o "deltaTime" para que a aceleração da bala não fosse tão grande.

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+    public float speed = 1f;
+    float mass = 500;
+    float force = 1000;
+    float acceleration;
+
 
     void OnCollisionEnter(Collision col)
     {
@@ -25,6 +31,8 @@ public class Shell : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-
+        acceleration = force / mass;
+        speed += acceleration + Time.deltaTime;
+        this.transform.Translate(0, 0, Time.deltaTime * speed * 0.5f);
     }
 }
